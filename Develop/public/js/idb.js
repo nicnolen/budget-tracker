8d7c6603cb835ed33b8c38ab98fb88808d2c22b2
Nicholas Nolen <nicnolen@ymail.com>
2022-03-16 15:35:54 -0500

    finalized the connection to IndexedDB

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -10,3 +10,21 @@ request.onupgradeneeded = function (event) {
   // create an object store (table) called `budget_tracker`, set it to have an auto incrementing primary key of sorts
   db.createObjectStore('budget_tracker', { autoIncrement: true });
 };
+
+// Upon a successful creation
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run uploadBudget function to send all local db data to api
+  if (navigator.onLine) {
+    // havnt created this yet
+    // uploadBudget
+  }
+};
+
+// If anything goes wrong with the database
+request.onerror = function (event) {
+  // log error here
+  console.error(event.target.errorCode);
+};
