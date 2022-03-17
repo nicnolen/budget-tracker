0e97762a877f363d29f5ae00861cf9f2fb593f47
Nicholas Nolen <nicnolen@ymail.com>
2022-03-17 16:12:25 -0500

    activated the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,6 +9,7 @@ const FILES_TO_CACHE = [
   "./js/idb.js",
 ];
 
+// Cache resources and install the service worker
 self.addEventListener('install', function (e) {
   e.waitUntil(
     // find the cache by name and then add every file in the `FILES_TO_CACHE` array to the cache
@@ -18,3 +19,27 @@ self.addEventListener('install', function (e) {
     })
   );
 }); // service workers run before the window object is created so we use self to instantiate listeners on the service worker
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
