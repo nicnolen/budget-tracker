c85591bd977bb0eab6a59aa46589324f5aba8169
Nicholas Nolen <nicnolen@ymail.com>
2022-03-18 12:45:48 -0500

    changed the folder names in the dist/client folder

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ const frontConfig = {
                 return '[path][name].[ext]';
               },
               publicPath: function (url) {
-                return url.replace('./', '/icons');
+                return url.replace('../images', '/icons');
               },
             },
           },
@@ -77,49 +77,49 @@ const frontConfig = {
           src: path.resolve('public/images/icons/icon-72x72.png'),
           sizes: '72x72',
           type: 'image/png',
-          destination: path.join('public', 'icons'),
+          destination: 'icons',
         },
         {
           src: path.resolve('public/images/icons/icon-96x96.png'),
           sizes: '96x96',
           type: 'image/png',
-          destination: path.join('public', 'icons'),
+          destination: 'icons',
         },
         {
           src: path.resolve('public/images/icons/icon-128x128.png'),
           sizes: '128x128',
           type: 'image/png',
-          destination: path.join('public', 'icons'),
+          destination: 'icons',
         },
         {
           src: path.resolve('public/images/icons/icon-144x144.png'),
           sizes: '144x144',
           type: 'image/png',
-          destination: path.join('public', 'icons'),
+          destination: 'icons',
         },
         {
           src: path.resolve('public/images/icons/icon-152x152.png'),
           sizes: '152x152',
           type: 'image/png',
-          destination: path.join('public', 'icons'),
+          destination: 'icons',
         },
         {
           src: path.resolve('public/images/icons/icon-192x192.png'),
           sizes: '192x192',
           type: 'image/png',
-          destination: path.join('public', 'icons'),
+          destination: 'icons',
         },
         {
           src: path.resolve('public/images/icons/icon-384x384.png'),
           sizes: '384x384',
           type: 'image/png',
-          destination: path.join('public', 'icons'),
+          destination: 'icons',
         },
         {
           src: path.resolve('public/images/icons/icon-512x512.png'),
           sizes: '512x512',
           type: 'image/png',
-          destination: path.join('public', 'icons'),
+          destination: 'icons',
         },
       ],
     }),
