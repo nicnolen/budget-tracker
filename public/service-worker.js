c10e049eff5a190c58a798916a27197ec79b662e
Nicholas Nolen <nicnolen@ymail.com>
2022-03-18 16:29:05 -0500

    scrapped webpack for MVP and made a PWA using the server

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,9 +4,18 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   './index.html',
-  './public/css/styles.css',
-  './dist/client/app.bundle.js',
-  './dist/client/idb.bundle.js',
+  './css/styles.css',
+  './js/index.js',
+  './js/idb.js',
+  './manifest.json',
+  './images/icons/icon-512x512.png',
+  './images/icons/icon-384x384.png',
+  './images/icons/icon-192x192.png',
+  './images/icons/icon-152x152.png',
+  './images/icons/icon-144x144.png',
+  './images/icons/icon-128x128.png',
+  './images/icons/icon-96x96.png',
+  './images/icons/icon-72x72.png',
 ];
 
 // Cache resources and install the service worker
