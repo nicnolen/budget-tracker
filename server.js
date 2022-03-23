db7b3a6a567016e8fa197a1b2ec1ccde8f113a3c
Nicholas Nolen <nicnolen@ymail.com>
2022-03-23 14:08:47 -0500

    changed start_url in manifest.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
 
-let PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget';
 
 const app = express();
